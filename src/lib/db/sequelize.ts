bb955a44763a115ea1d67fe43a8631515b9cb7b2
Jerry <bnb1083@gmail.com>
2024-01-10 14:54:14 +0800

    Config cloud run sql connection

--- src/lib/db/sequelize.ts
+++ src/lib/db/sequelize.ts
@@ -1,15 +1,36 @@
-import { Sequelize } from 'sequelize';
+import {Sequelize, Options} from 'sequelize';
 
 const dbHost = process.env.DB_HOST;
 const dbUser = process.env.DB_USER;
 const dbPassword = process.env.DB_PASSWORD;
+const env = process.env.RUNTIME_ENV
+const cloudSqlSocketPath = process.env.CLOUD_SQL_SOCKET_PATH;
 
-const sequelize = new Sequelize({
+const dbOption: Options = {
     database: 'postgres',
     username: dbUser,
     password: dbPassword,
     host: dbHost,
     dialect: 'postgres',
-});
+}
+
+if (env === 'cloud_run') {
+    dbOption.dialectOptions = {
+        socketPath: cloudSqlSocketPath,
+    }
+}
+
+const sequelize = new Sequelize(dbOption);
+
+if (env === 'cloud_run') {
+    sequelize
+        .authenticate()
+        .then(() => {
+            console.log("cloud sql connection has been established successfully.");
+        })
+        .catch((err) => {
+            console.error("Unable to connect to the cloud sql database:", err);
+        });
+}
 
 export default sequelize;
