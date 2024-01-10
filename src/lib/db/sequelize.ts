86cfe8931b4d79125ad9dec80d2649e533b92e1a
Jerry <bnb1083@gmail.com>
2024-01-10 14:35:12 +0800

    Remove default config

--- src/lib/db/sequelize.ts
+++ src/lib/db/sequelize.ts
@@ -1,8 +1,8 @@
 import { Sequelize } from 'sequelize';
 
-const dbHost = process.env.DB_HOST || 'localhost';
-const dbUser = process.env.DB_USER || 'root';
-const dbPassword = process.env.DB_PASSWORD || '<PASSWORD>';
+const dbHost = process.env.DB_HOST;
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASSWORD;
 
 const sequelize = new Sequelize({
     database: 'postgres',
