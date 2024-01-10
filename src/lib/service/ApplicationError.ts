56b36df91d902e6dda43708d41070bfce3d6dab1
Jerry <bnb1083@gmail.com>
2024-01-10 12:26:57 +0800

    Create service level error

---
+++ src/lib/service/ApplicationError.ts
@@ -0,0 +1,10 @@
+class ApplicationError extends Error {
+    code: number;
+
+    constructor(code: number, message: string) {
+        super(message);
+        this.code = code;
+    }
+}
+
+export default ApplicationError
