3053172d1e44456018cbff5059fcdbb1fb69b904
Jerry <bnb1083@gmail.com>
2024-01-10 14:54:01 +0800

    Create validation utils

---
+++ src/lib/util/validation.ts
@@ -0,0 +1,8 @@
+export function isValidPassword(password: string | undefined): boolean {
+    if (password === undefined) {
+        return false;
+    }
+
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    return passwordRegex.test(password);
+}
