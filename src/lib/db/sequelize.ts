d8f25d03da8b41fa09fa129cb2c2354ad373e2bd
Jerry <bnb1083@gmail.com>
2024-01-11 12:38:22 +0800

    Try mysql database

--- src/lib/db/sequelize.ts
+++ src/lib/db/sequelize.ts
@@ -1,17 +1,19 @@
 import {Sequelize, Options} from 'sequelize';
 
+const database = process.env.DB_NAME;
 const dbHost = process.env.DB_HOST;
 const dbUser = process.env.DB_USER;
 const dbPassword = process.env.DB_PASSWORD;
+
 const env = process.env.RUNTIME_ENV
 const cloudSqlSocketPath = process.env.CLOUD_SQL_SOCKET_PATH;
 
 let dbOption: Options = {
-    database: 'postgres',
+    database: database,
     username: dbUser,
     password: dbPassword,
     host: dbHost,
-    dialect: 'postgres',
+    dialect: 'mysql',
 }
 
 if (env === 'cloud_run') {
