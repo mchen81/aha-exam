e87a879c033fd3ef0bedab5bee031bb0dd5f2716
Jerry <bnb1083@gmail.com>
2024-01-14 17:44:58 +0800

    Lint file

--- src/lib/service/ApplicationError.ts
+++ src/lib/service/ApplicationError.ts
@@ -1,10 +1,10 @@
 class ApplicationError extends Error {
-    code: number;
+  code: number
 
-    constructor(code: number, message: string) {
-        super(message);
-        this.code = code;
-    }
+  constructor (code: number, message: string) {
+    super(message)
+    this.code = code
+  }
 }
 
 export default ApplicationError
--- src/lib/util/validation.ts
+++ src/lib/util/validation.ts
@@ -1,8 +1,13 @@
-export function isValidPassword(password: string | undefined): boolean {
-    if (password === undefined) {
-        return false;
-    }
+/**
+ * Checks if a given password is valid.
+ * @param {string} password - The password to be checked.
+ * @returns {boolean} - Whether the password is valid or not.
+ */
+export function isValidPassword (password: string | undefined): boolean {
+  if (password === undefined) {
+    return false
+  }
 
-    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
-    return passwordRegex.test(password);
+  const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
+  return passwordRegex.test(password)
 }
