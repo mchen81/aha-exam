5116ed79ccea10ba6ff0a09591a8898ea783deca
Jerry <bnb1083@gmail.com>
2024-01-15 22:41:32 +0800

    Implement logout api

---
+++ src/pages/api/auth/logout.ts
@@ -0,0 +1,29 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import ApplicationError from '@/lib/service/ApplicationError'
+import UserAuthService from '@/lib/service/UserAuthService'
+import { createRouter } from 'next-connect'
+import { getSessionTokenFromCookie, setCookieForSession } from '@/util/http'
+
+const router = createRouter<NextApiRequest, NextApiResponse>()
+const userAuthService = UserAuthService.getInstance()
+router.post(async (req, res, next) => {
+  const sessionToken = getSessionTokenFromCookie(req)
+  if (sessionToken === null) {
+    res.status(401).json({ error: 'Unauthorized' })
+    return
+  }
+  await userAuthService.logoutUser(sessionToken)
+  setCookieForSession(res, '')
+  res.status(200).json({})
+})
+
+export default router.handler({
+  onError: (err: any, req: any, res: any) => {
+    if (err instanceof ApplicationError) {
+      res.status(400).json({ error: err.message })
+    } else {
+      console.log(err)
+      res.status(500).json({ error: 'Internal Server Error' })
+    }
+  }
+})
