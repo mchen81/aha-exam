097068f3b8454bd016057dea24e9a36ea3678249
Jerry <bnb1083@gmail.com>
2024-01-15 22:40:48 +0800

    Use auth provider globally

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import type { AppProps } from 'next/app'
 import { AppCacheProvider } from '@mui/material-nextjs/v13-pagesRouter'
+import { AuthProvider } from '@/context/AuthContext'
 
 export default function App ({ Component, pageProps: { session, ...pageProps } }: AppProps): React.JSX.Element {
   return (
-    <AppCacheProvider {...pageProps}>
-      <Component {...pageProps} />
-    </AppCacheProvider>
+    <AuthProvider>
+      <AppCacheProvider {...pageProps}>
+        <Component {...pageProps} />
+      </AppCacheProvider>
+    </AuthProvider>
   )
 }
