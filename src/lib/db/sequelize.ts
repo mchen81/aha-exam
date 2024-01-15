3852968a8ea8d9f72e3d88576f350d2b80cd42f6
Jerry <bnb1083@gmail.com>
2024-01-15 22:43:37 +0800

    lint file

--- src/lib/db/sequelize.ts
+++ src/lib/db/sequelize.ts
@@ -1,41 +1,39 @@
-import {Sequelize, Options} from 'sequelize';
+import { Sequelize, type Options } from 'sequelize'
 
-const database = process.env.DB_NAME;
-const dbHost = process.env.DB_HOST;
-const dbUser = process.env.DB_USER;
-const dbPassword = process.env.DB_PASSWORD;
+const database = process.env.DB_NAME
+const dbHost = process.env.DB_HOST
+const dbUser = process.env.DB_USER
+const dbPassword = process.env.DB_PASSWORD
 
 const env = process.env.RUNTIME_ENV
-const cloudSqlSocketPath = process.env.CLOUD_SQL_SOCKET_PATH;
+const cloudSqlSocketPath = process.env.CLOUD_SQL_SOCKET_PATH
 
 let dbOption: Options = {
-    database: database,
-    username: dbUser,
-    password: dbPassword,
-    host: dbHost,
-    dialect: 'mysql',
+  database,
+  username: dbUser,
+  password: dbPassword,
+  host: dbHost,
+  dialect: 'mysql'
 }
 
 if (env === 'cloud_run') {
-    dbOption = {
-        ...dbOption,
-        dialectOptions: {
-            socketPath: cloudSqlSocketPath,
-        }
+  dbOption = {
+    ...dbOption,
+    dialectOptions: {
+      socketPath: cloudSqlSocketPath
     }
+  }
 }
 
-const sequelize = new Sequelize(dbOption);
+const sequelize = new Sequelize(dbOption)
 
-if (env === 'cloud_run') {
-    sequelize
-        .authenticate()
-        .then(() => {
-            console.log('cloud sql connection has been established successfully.');
-        })
-        .catch((err) => {
-            console.error(`Unable to connect to the cloud sql with socket path ${cloudSqlSocketPath}:`, err);
-        });
-}
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('cloud sql connection has been established successfully.')
+  })
+  .catch((err) => {
+    console.error(`Unable to connect to the cloud sql with socket path ${cloudSqlSocketPath}:`, err)
+  })
 
-export default sequelize;
+export default sequelize
