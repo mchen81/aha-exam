983ed7cf073bc642938d7186bb3f5cf43fd40eb3
Jerry <bnb1083@gmail.com>
2024-01-15 22:47:32 +0800

    Create api for getting user metadata

---
+++ src/pages/api/user/index.ts
@@ -0,0 +1,26 @@
+import { createRouter } from 'next-connect'
+import type { NextApiRequest, NextApiResponse } from 'next'
+import UserAccountService from '@/lib/service/UserAccountService'
+import UserAuthService from '@/lib/service/UserAuthService'
+
+import { getSessionTokenFromCookie } from '@/util/http'
+
+const router = createRouter<NextApiRequest, NextApiResponse>()
+
+const userAccountService = UserAccountService.getInstance()
+const userAuthService = UserAuthService.getInstance()
+
+router.get(async (req, res, next) => {
+  const sessionToken = getSessionTokenFromCookie(req)
+  if (sessionToken === null) {
+    res.status(401).json({ message: 'Unauthorized' }); return
+  }
+
+  const user = await userAuthService.getUserBySessionToken(sessionToken)
+
+  const result = await userAccountService.getUserByEmail(user.email)
+
+  res.json(result)
+})
+
+export default router.handler()
