fd433c3b26f71a691e84d74482c35769e4c539bc
Jerry <bnb1083@gmail.com>
2024-01-15 22:46:31 +0800

    Create page for waiting user verify email

---
+++ src/pages/user/verify.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Container } from '@mui/material'
+
+const Verify: React.FC = () => {
+  return (
+    <Container maxWidth="sm">
+      Thank you for your registration, we have sent you an email to verify your account.
+    </Container>
+  )
+}
+
+export default Verify
---
+++ src/pages/user/wait-verify.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Container } from '@mui/material'
+
+const WaitVerify: React.FC = () => {
+  return (
+    <Container maxWidth="sm">
+      You are waiting for verification.
+    </Container>
+  )
+}
+
+export default WaitVerify
