2341ca483ac939b2f73fdb681c41bc2a62984871
Jerry <bnb1083@gmail.com>
2024-01-22 22:23:04 +0800

    chore: add cors policy

--- next.config.js
+++ next.config.js
@@ -1,6 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          {key: 'Access-Control-Allow-Credentials', value: 'true'},
+          {key: 'Access-Control-Allow-Origin', value: process.env.SERVER_HOST},
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET,DELETE,POST,PUT,OPTIONS',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'Content-Length,Content-Type',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
